c9e2ae194a99f0a41c4a0c076d988fd817509f61
Matilda Mared <matildamared@live.se>
2023-02-27 07:01:38 +0100

    work on product service

--- product-service/src/main/java/com/matildamared/productservice/service/ProductService.java
+++ product-service/src/main/java/com/matildamared/productservice/service/ProductService.java
@@ -1,16 +1,45 @@
 package com.matildamared.productservice.service;
 
 import com.matildamared.productservice.dto.ProductRequest;
+import com.matildamared.productservice.dto.ProductResponse;
 import com.matildamared.productservice.model.Product;
+import com.matildamared.productservice.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
+@Slf4j
 public class ProductService {
+
+    private final ProductRepository productRepository;
     public void createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.getName())
                 .description(productRequest.getDescription())
                 .price(productRequest.getPrice())
+                .build();
+
+        productRepository.save(product);
+
+        log.info("Product created with id {}", product.getId());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        List<Product> products = productRepository.findAll();
+
+        return products.stream().map(this::mapToProductResponse).toList();
+    }
+
+    private ProductResponse mapToProductResponse(Product product) {
+        return ProductResponse.builder()
+                .id(product.getId())
+                .name(product.getName())
+                .description(product.getDescription())
+                .price(product.getPrice())
                 .build();
     }
 }
