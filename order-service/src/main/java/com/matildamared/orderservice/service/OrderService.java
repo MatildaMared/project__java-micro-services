e4d8679f569e1e3986bef2318d6d66c71d9b6c33
Matilda Mared <matildamared@live.se>
2023-03-02 07:30:10 +0100

    work on order service

---
+++ order-service/src/main/java/com/matildamared/orderservice/service/OrderService.java
@@ -0,0 +1,41 @@
+package com.matildamared.orderservice.service;
+
+import com.matildamared.orderservice.dto.OrderLineItemDto;
+import com.matildamared.orderservice.dto.OrderRequest;
+import com.matildamared.orderservice.model.Order;
+import com.matildamared.orderservice.model.OrderLineItems;
+import com.matildamared.orderservice.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+
+        List<OrderLineItems> orderLineItems = orderRequest.getOrderLineItemsDtoList()
+                .stream()
+                .map(this::mapToDto)
+                .toList();
+
+        order.setOrderLineItemsList(orderLineItems);
+
+        orderRepository.save(order);
+    }
+
+    private OrderLineItems mapToDto(OrderLineItemDto orderLineItemDto) {
+        OrderLineItems orderLineItems = new OrderLineItems();
+        orderLineItems.setSkuCode(orderLineItemDto.getSkuCode());
+        orderLineItems.setPrice(orderLineItemDto.getPrice());
+        orderLineItems.setQuantity(orderLineItemDto.getQuantity());
+        return orderLineItems;
+    }
+}
